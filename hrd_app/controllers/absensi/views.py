a36ff0a4a20815bd500ee7e467e1646084a38b0b
azril sugiarto <azrilsugiarto06@gmail.com>
2024-11-21 15:12:55 +0700

    update

--- hrd_app/controllers/absensi/views.py
+++ hrd_app/controllers/absensi/views.py
@@ -29,11 +29,11 @@ def absensi(r,sid):
         
         status = status_pegawai_db.objects.using(r.session["ccabang"]).all().order_by('id')
         
-        ###
+        ##
         try:
             sid_lembur = status_pegawai_lembur_db.objects.using(r.session["ccabang"]).get(status_pegawai_id = sid)
             sid_lembur = sid_lembur.status_pegawai.pk
-        except:
+        except:  
             sid_lembur = 0
         jenis_ijin = jenis_ijin_db.objects.using(r.session["ccabang"]).all()   
         data = {
