3288336d7f21f633157bdb801d48fdacdcb5505e
azril sugiarto <azrilsugiarto06@gmail.com>
2024-11-29 14:01:33 +0700

    update

--- hrd_app/controllers/lembur/views.py
+++ hrd_app/controllers/lembur/views.py
@@ -13,9 +13,9 @@ def lembur(r, sid):
         dsid = dakses.sid_id
         
         today = date.today()
-        pa = periode_tgl(today)
-        dari = pa[0].date()
-        sampai = pa[1].date()
+        pa = periode_absen(today.month,today.year)
+        dari = datetime.strptime(pa[0].date().strftime("%d-%m-%Y"),"%d-%m-%Y").date()
+        sampai = datetime.strptime(pa[1].date().strftime("%d-%m-%Y"),"%d-%m-%Y").date()
         prd = pa[2]
         thn = pa[3]
         bln = nama_bulan(int(prd))
