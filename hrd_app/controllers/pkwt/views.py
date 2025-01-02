c29d41125a61891e76c9f7550f21489a2c55d4f1
azril sugiarto <azrilsugiarto06@gmail.com>
2025-01-02 10:45:44 +0700

    update

--- hrd_app/controllers/pkwt/views.py
+++ hrd_app/controllers/pkwt/views.py
@@ -16,6 +16,8 @@ def pkwt(r):
 @login_required
 def pkwt_json(r):
     try:
+        cabang = r.session["ccabang"]
+        static = "http://localhost:8006/static"
         if r.method == "POST":
             id_user = r.user.id
             print("Ok")
@@ -43,6 +45,7 @@ def pkwt_json(r):
                     sampai = r.POST.get("sampai")
                     tempat = r.POST.get("tempat")
                     gaji = r.POST.get("gaji")
+                    cbg = r.POST.get("cabang")
                     bank = r.POST.get("bank")
                     darif = datetime.strptime(dari,"%d-%m-%Y")
                     sampaif = datetime.strptime(sampai,"%d-%m-%Y")
@@ -53,19 +56,21 @@ def pkwt_json(r):
                     gaji = "{:,}".format(int(gaji))
 
 
-                    html = get_template("hrd_app/pkwt/format.html")
-                    data = {"nama1":nama1,"jabatan1":jabatan1, "nama2":nama2, "ttl2":ttl2, "noktp2":noktp2, "alamat2":alamat2, "nohp2":nohp2, "jabatan2":jabatan2, "tugas":tugas, "jangka":jangka, "tipe":tipe, "dari":dr, "sampai":sp, "tempat":tempat, "gaji":gaji, "bank":bank,"hari":hari,"tanggal":tanggal,"bulan":bulan,"tahun":tahun,"terbilang":bilang}
+                    data = {"nama1":nama1,"jabatan1":jabatan1, "nama2":nama2, "ttl2":ttl2, "noktp2":noktp2, "alamat2":alamat2, "nohp2":nohp2, "jabatan2":jabatan2, "tugas":tugas, "jangka":jangka, "tipe":tipe, "dari":dr, "sampai":sp, "tempat":tempat, "gaji":gaji, "bank":bank,"hari":hari,"tanggal":tanggal,"bulan":bulan,"tahun":tahun,"terbilang":bilang,"cabang":cbg,"static":static}
+                    # return render(r,f"hrd_app/pkwt/{cabang}/format.html",data)
+                    html = get_template(f"hrd_app/pkwt/{cabang}/format.html")
                     ctx = html.render(data)
                     html = weasyprint.HTML(string=ctx)
                     css = weasyprint.CSS(filename="static/bootstrap/css/bootstrap.min.css")
-                    file = html.write_pdf("static/pdf/pkwt.pdf",stylesheets=[css])
+                    file = html.write_pdf(f"static/pdf/pkwt_{cabang}.pdf",stylesheets=[css])
                     return redirect("pkwt_json")
         else:
-            with open("static/pdf/pkwt.pdf","rb") as f:
+            with open(f"static/pdf/pkwt_{cabang}.pdf","rb") as f:
                 response = HttpResponse(f.read(),"application/pdf")
-                response["Content-Dispotion"] = 'filename=pkwt.pdf'
+                response["Content-Dispotion"] = f'filename=pkwt_{cabang}.pdf'
                 return response
     except Exception as e:
+        print(e)
         messages.error(r,"Terjadi kesalahan")
         return redirect("pkwt")
         
